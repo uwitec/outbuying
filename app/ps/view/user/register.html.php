5beb87edf478c3d506375d8255b381e00e6c4695
ljxzhy <ljxzhy@1384f862-e307-619e-4894-297cafbc766c>
2010-09-25 10:04:30 +0000

    重新改写用户注册

--- app/ps/view/user/register.html.php
+++ app/ps/view/user/register.html.php
@@ -3,11 +3,6 @@
  * 用户注册
  */
 ?>
-<style>
-.uDiv{margin:0,0,5px,0;}
-.uSpan{float:left;margin:0,0,0,5px}
-label{color:red}
-</style>
 <script>
 function checkForm()
 {
@@ -62,95 +57,75 @@ function checkForm()
 	}
 }
 </script>
+<style>
+.nameDiv{width:100px;float:left}
+.vDiv{width:150px;float:left}
+.infoDiv{width:300px;float:left}
+.mDiv{clear:both}
+.mainDiv{margin-left:200px}
+.submitDiv{padding-left:0px}
+</style>
 <form action="" method="POST" onsubmit="return checkForm();">
 <center>
-<div  style="width:300px">
-<fieldset>
-     <legend><?=Watt_I18n::trans("用户注册")?></legend>
-         <br>
-          <div class="uDiv">
-              <span class="uSpan">
-                  　　<label>*</label><?=Watt_I18n::trans("姓名")?>:
-              </span>
-              <span>
-                  <input type="text" name="u_name" id="u_name">
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan">
-                  　　<label>*</label><?=Watt_I18n::trans("昵称")?>:
-              </span>
-              <span>
-                  <input type="text" name="u_nickname" id="u_nickname">
-              </span>
-          </div>
-           <div class="uDiv">
-              <span class="uSpan">
-                  　　&nbsp;<?=Watt_I18n::trans("称谓")?>:
-              </span>
-              <span>
-                  <input type="radio" name="u_sex" id="u_sex" value="1" checked><?=Watt_I18n::trans("先生")?>
-                  <input type="radio" name="u_sex" id="u_sex" value="2"><?=Watt_I18n::trans("女士")?>
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan">
-                  　　<label>*</label><?=Watt_I18n::trans("密码")?>:
-              </span>
-              <span>
-                  <input type="password" name="u_pwd" id="u_pwd">
-              </span>
-          </div>
-           <div class="uDiv">
-              <span class="uSpan" >
-                  <label>*</label><?=Watt_I18n::trans("密码重复")?>:
-              </span>
-              <span>
-                  <input type="password" name="u_pwd_1" id="u_pwd_1">
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan" >
-                  　　&nbsp;&nbsp;<?=Watt_I18n::trans("电话")?>
-              </span>
-              <span>
-                  <input type="text" name="u_phone" id="u_phone">
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan" >
-                  　　<label>*</label><?=Watt_I18n::trans("手机")?>
-              </span>
-              <span>
-                  <input type="text" name="u_mobile" id="u_mobile">
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan" >
-                  　　<label>*</label><?=Watt_I18n::trans("地址")?>
-              </span>
-              <span>
-                  <input type="text" name="u_address" id="u_address">
-              </span>
-          </div>
-          <div class="uDiv">
-              <span class="uSpan" >
-                  　　&nbsp;<?=Watt_I18n::trans("邮箱")?>
-              </span>
-              <span>
-                  <input type="text" name="u_email" id="u_email">
-              </span>
-          </div>
-          <div align="center" class="uDiv">
-          <span>
-          <input type="submit" value="<?=Watt_I18n::trans("注册")?>">
-          　
-          <input type="reset" value="<?=Watt_I18n::trans("取消")?>">
-          <input type="hidden" name="op" value="1">
-          </span>
-          </div>
-         
-</fieldset>
+<div class='mainDiv'>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("用户名")?>:</div>
+		<div class="vDiv"><input type="text" name="u_name" id="u_name"></div>
+		<div class="infoDiv"><div>请填写用户的名称</div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("昵称")?>:</div>
+		<div class="vDiv"><input type="text" name="u_nickname" id="u_nickname"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("称谓")?>:</div>
+		<div class="vDiv">
+		<input type="radio" name="u_sex" id="u_sex" value="1" checked><?=Watt_I18n::trans("先生")?>
+        <input type="radio" name="u_sex" id="u_sex" value="2"><?=Watt_I18n::trans("女士")?>
+		</div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("密码")?>:</div>
+		<div class="vDiv"><input type="text" name="u_pwd" id="u_pwd"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("再次输入密码")?>:</div>
+		<div class="vDiv"><input type="text" name="u_pwd_1" id="u_pwd_1"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("电话")?>:</div>
+		<div class="vDiv"><input type="text" name="u_phone" id="u_phone"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("手机")?>:</div>
+		<div class="vDiv"><input type="text" name="u_mobile" id="u_mobile"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("地址")?>:</div>
+		<div class="vDiv"><input type="text" name="u_address" id="u_address"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div class="mDiv">
+		<div class="nameDiv"><?=Watt_I18n::trans("邮箱")?>:</div>
+		<div class="vDiv"><input type="text" name="u_email" id="u_email"></div>
+		<div class="infoDiv"><div></div></div>
+	</div>
+	<div  class="mDiv" >
+	<div class='submitDiv'>
+		<input type="submit" value="<?=Watt_I18n::trans("注册")?>">　
+        <input type="reset" value="<?=Watt_I18n::trans("取消")?>">
+        <input type="hidden" name="op" value="1">
+	</div>
+		
+	</div>
+
+
 </div>
 </center>
 </form>
