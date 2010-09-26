02df16f1c81e79f5fe4558b05d4478287002b86b
newhesper <newhesper@1384f862-e307-619e-4894-297cafbc766c>
2010-09-26 12:51:55 +0000

    调用类使用pft_*

--- trunk/app/ps/view/user/register.html.php
+++ trunk/app/ps/view/user/register.html.php
@@ -234,56 +234,56 @@ td{height:31px}
 <table width="100%" border=0 cellpadding="0" cellspacing="0">
 	<tr id="nameTr">
 	
-	<td width='15%'><?=Watt_I18n::trans("会员名")?>:</td>
+	<td width='15%'><?=Pft_I18n::trans("会员名")?>:</td>
 	<td width='30%'><input type="text" name="u_name" id="u_name" onfocus="viewtixing('name')" onblur="checkvalue('u_name')" >&nbsp;<img src='' id="nameImg" style="display:none" /></td>
 	<td width="55%"><div class='infoDiv' id="nameDiv" style="display:none">&nbsp;5-20个字符,只允许 数字、字母、汉字、-_,推荐使用汉字.</div></td>
 	
 	</tr>
 	<tr id="nicknameTr">
-	<td><?=Watt_I18n::trans("昵称")?>:</td>
+	<td><?=Pft_I18n::trans("昵称")?>:</td>
 	<td><input type="text" name="u_nickname" id="u_nickname" onfocus="viewtixing('nickname')" onblur="checkvalue('u_nickname')">&nbsp;<img src='' id="nicknameImg" style="display:none" /></td>
 	<td><div class='infoDiv' id="nicknameDiv" style="display:none">&nbsp;请输入您的昵称.</div></td>
 	</tr>
 	<tr id="sexTr">
-	<td><?=Watt_I18n::trans("称谓")?>:</td>
-	<td><input type="radio" name="u_sex" id="u_sex" value="1"><?=Watt_I18n::trans("先生")?>
-        <input type="radio" name="u_sex" id="u_sex" value="2" checked ><?=Watt_I18n::trans("女士")?>
+	<td><?=Pft_I18n::trans("称谓")?>:</td>
+	<td><input type="radio" name="u_sex" id="u_sex" value="1"><?=Pft_I18n::trans("先生")?>
+        <input type="radio" name="u_sex" id="u_sex" value="2" checked ><?=Pft_I18n::trans("女士")?>
 		<img src='' id="sexImg" style="display:none"/>
 		</td>
 	<td><div class='infoDiv' id="nicknameDiv" style="display:none">&nbsp;请选择您的称谓.</div></td>
 	</tr>
 	<tr id="pwdTr">
-	<td><?=Watt_I18n::trans("密码")?>:</td>
+	<td><?=Pft_I18n::trans("密码")?>:</td>
 	<td><input type="password" name="u_pwd" id="u_pwd" onfocus="viewtixing('pwd')" onblur="checkvalue('u_pwd')">&nbsp;<img src='' id="pwdImg" style="display:none"/></td>
 	<td><div class='infoDiv' id="pwdDiv" style="display:none">&nbsp;请输入您的密码,5-20个字符,只允许 数字、字母.</div></td>
 	</tr>
 	<tr id="pwd_1Tr">
-	<td><?=Watt_I18n::trans("确认密码")?>:</td>
+	<td><?=Pft_I18n::trans("确认密码")?>:</td>
 	<td><input type="password" name="u_pwd_1" id="u_pwd_1" onfocus="viewtixing('pwd_1')" onblur="checkvalue('u_pwd_1')">&nbsp;<img src='' id="pwd_1Img" style="display:none"/></td>
 	<td><div class='infoDiv' id="pwd_1Div" style="display:none">&nbsp;请再次输入您的密码.</div></td>
 	</tr>
 	<tr id="phoneTr">
-	<td><?=Watt_I18n::trans("电话")?>:</td>
+	<td><?=Pft_I18n::trans("电话")?>:</td>
 	<td><input type="text" name="u_phone" id="u_phone" onfocus="viewtixing('phone')" onblur="checkvalue('u_phone')">&nbsp;<img src='' id="phoneImg" style="display:none" /></td>
 	<td><div class='infoDiv' id="phoneDiv" style="display:none">&nbsp;请留下您的电话号码且必须符合电话号码的格式,以便于及时沟通.</div></td>
 	</tr>
 	<tr id="mobileTr">
-	<td><?=Watt_I18n::trans("手机")?>:</td>
+	<td><?=Pft_I18n::trans("手机")?>:</td>
 	<td><input type="text" name="u_mobile" id="u_mobile" onfocus="viewtixing('mobile')" onblur="checkvalue('u_mobile')">&nbsp;<img src='' id="mobileImg" style="display:none"/></td>
 	<td><div class='infoDiv' id="mobileDiv" style="display:none">&nbsp;请留下您的手机号码且必须符合手机号码的格式,以便于及时沟通.</div></td>
 	</tr>
 	<tr id="addressTr">
-	<td><?=Watt_I18n::trans("地址")?>:</td>
+	<td><?=Pft_I18n::trans("地址")?>:</td>
 	<td><input type="text" name="u_address" id="u_address" onfocus="viewtixing('address')" onblur="checkvalue('u_address')">&nbsp;<img src='' id="addressImg" style="display:none"/></td>
 	<td><div class='infoDiv' id="addressDiv" style="display:none">&nbsp;请正确输入您的详细地址,以便于能够及时找到</div></td>
 	</tr>
 	<tr id="emailTr">
-	<td><?=Watt_I18n::trans("邮箱")?>:</td>
+	<td><?=Pft_I18n::trans("邮箱")?>:</td>
 	<td><input type="text" name="u_email" id="u_email" onfocus="viewtixing('email')" onblur="checkvalue('u_email')">&nbsp;<img src='' id="emailImg" style="display:none"/></td>
 	<td><div class='infoDiv' id="emailDiv" style="display:none">&nbsp;请输入您的邮箱且必须符合邮箱的格式</div></td>
 	</tr>
 	<tr id="emailTr">
-	<td><?=Watt_I18n::trans("验证码")?>:</td>
+	<td><?=Pft_I18n::trans("验证码")?>:</td>
 	<td><input type="text" name="u_yanzhengma" id="u_yanzhengma" onfocus="viewtixing('email')" onblur="checkvalue('u_yanzhengma')">&nbsp;<img src='' id="yanzhengmaImg" style="display:none"/>
 	<!--<img src="?do=ps_user_yanzhengma"  id="safecode" onclick="reloadcode();"/>-->
 	</td>
@@ -293,14 +293,14 @@ td{height:31px}
 		<td colspan=3 align='left'>
 		<br>
 		<div style="padding-left:100px">
-			<input type="submit" value="<?=Watt_I18n::trans("注册")?>">
-          <input type="reset" value="<?=Watt_I18n::trans("取消")?>">
+			<input type="submit" value="<?=Pft_I18n::trans("注册")?>">
+          <input type="reset" value="<?=Pft_I18n::trans("取消")?>">
           <input type="hidden" name="op" value="1">
 		 </div>
 		</td>
 	</tr>
 </table>
-<div>
+</div>
 </div>
 </center>
 </form>
