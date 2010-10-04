000f1a33b1f361ca0e868d0f53bf3c99ba47789a
newhesper <newhesper@1384f862-e307-619e-4894-297cafbc766c>
2010-10-04 12:07:39 +0000

    没有配置只读db时使用默认db

--- trunk/config/default/db_srv.conf.php
+++ trunk/config/default/db_srv.conf.php
@@ -9,9 +9,9 @@ return array (
       array (
         'phptype' => 'mysql',
         'hostspec' => 'localhost',
-        'database' => 'pftninni_ofoho',
-        'username' => 'pftninni_ofoho',
-        'password' => 'FCt;9-Kc!KLs',
+        'database' => 'pftninni_eding100',
+        'username' => 'pftninni_eding',
+        'password' => '2d$AQv*DRpXT',
         'port' => '',
       ),
     ),
