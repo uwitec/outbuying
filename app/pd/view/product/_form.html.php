7ac8f3fc56204b27c3c4fe6137d07f88b33aa8e0
ljxzhy <ljxzhy@1384f862-e307-619e-4894-297cafbc766c>
2010-10-25 13:22:12 +0000

    修改添加产品页面

--- app/pd/view/product/_form.html.php
+++ app/pd/view/product/_form.html.php
@@ -2,7 +2,7 @@
 //产品分类
 ?>
 <?
-//include( Pft_Config::getCfg('PATH_ROOT').'inc/view/header.inc.php' );
+include( Pft_Config::getCfg('PATH_ROOT').'inc/view/header.inc.php' );
 ?>
 <style>
 ._grid{border:1px solid #D4E8FC;}
@@ -12,7 +12,7 @@
 <script src="js/jquery.min.js" ></script>
 <script>
 $(document).ready(function(){
-	$.ajax({
+	/*$.ajax({
 	   type: "POST",
 	   url: "?do=tools_uploader_upload",
 	   data: "",
@@ -21,11 +21,77 @@ $(document).ready(function(){
 		 
 	   }
 	}
-	)
+	);
+	*/
+	
+	/*$("#s_Submit").bind('click',function(){
+		alert();
+		alert($("#p_name").val());
+		
+	});
+	*/
+	$('#imgDiv').load('?do=tools_uploader_upload');//加载上传文件页面
 }
 );
-
-
+function saveProduct()
+{
+	var p_name=document.getElementById("p_name");
+	var pPname=document.getElementById("pPname");
+	if(p_name.value=="")
+	{
+		pPname.innerHTML="请输入名称";
+		return false;
+	}
+	else
+	{
+		pPname.innerHTML="";
+	}
+	var p_price=document.getElementById("p_price");
+	var pPrice=document.getElementById("pPrice");
+	if(p_price.value=="")
+	{
+		pPrice.innerHTML="请输入价格";
+		return false;
+	}
+	else
+	{
+		pPrice.innerHTML="";
+	}
+	var p_market_price=document.getElementById("p_market_price");
+	var pMarketPrice=document.getElementById("pMarketPrice");
+	if(p_market_price.value=="")
+	{
+		pMarketPrice.innerHTML="请输入市场价格";
+		return false;
+	}
+	else
+	{
+		pMarketPrice.innerHTML="";
+	}
+	var p_mem_price=document.getElementById("p_mem_price");
+	var pMemPrice=document.getElementById("pMemPrice");
+	if(p_mem_price.value=="")
+	{
+		pMemPrice.innerHTML="请输入会员价格";
+		return false;
+	}
+	else
+	{
+		pMemPrice.innerHTML="";
+	}
+	var p_unit=document.getElementById("p_unit");
+	var pUnit=document.getElementById("pUnit");
+	if(p_unit.value=="")
+	{
+		pUnit.innerHTML="请输入会员价格";
+		return false;
+	}
+	else
+	{
+		pUnit.innerHTML="";
+	}
+	
+}
 </script>
 <table width="90%" align="center" cellpadding="0" cellspacing="0" class="_grid">
 <tr>
@@ -35,43 +101,52 @@ $(document).ready(function(){
   <div><b>添加产品</b></div>
   <div>
     <div>
-      <table width="100%" cellpadding="0" cellspacing="0" border="1">
+    <form action="" method="get">
+      <table width="100%" cellpadding="2" cellspacing="2" border="0">
         <tr>
           <td width="20%">名称</td>
           <td width="40%"><input type="text" id="p_name" name="p_name"></td>
-          <td width="40%"></td>
+          <td width="40%"><span id="pPname"></span></td>
         </tr>
          <tr>
           <td>价格</td>
           <td><input type="text" id="p_price" name="p_price"></td>
-          <td></td>
+          <td><span id="pPrice"></span></td>
         </tr>
         <tr>
           <td>市场价格</td>
           <td><input type="text" id="p_market_price" name="p_market_price"></td>
-          <td></td>
+          <td><span id="pMarketPrice"></span></td>
         </tr>
          <tr>
           <td>会员价格</td>
           <td><input type="text" id="p_mem_price" name="p_mem_price"></td>
-          <td></td>
+          <td><span id="pMemPrice"></span></td>
         </tr>
          <tr>
           <td>单位</td>
           <td><input type="text" id="p_unit" name="p_unit"></td>
-          <td></td>
+          <td><span id="pUnit"></span></td>
         </tr>
          <tr>
           <td>产品图片</td>
           <td><div id="imgDiv"></div></td>
-          <td></td>
+          <td><span id="pImg"></span></td>
         </tr>
          <tr>
           <td>产品说明</td>
-          <td><textarea id="p_info" name="p_info" rows="2" cols="20"></textarea></td>
-          <td></td>
+          <td><textarea id="p_info" name="p_info" rows="2" cols="30"></textarea></td>
+          <td><span id="pInfo"></span></td>
+        </tr>
+        <tr>
+        <td colspan="3" align="center">
+        <input type="submit" style="display:none" id="Submit">
+        <input type="hidden" name="op" value="1">
+        <input type="button" id="s_Submit" onclick="saveProduct();"  value="<?=Watt_I18n::trans("保存")?>"> 
+        </td>
         </tr>
       </table>
+      </form>
     </div>
   </div>
 </div>
@@ -84,5 +159,5 @@ aaaaaaaaa
 </tr>
 </table>
 <?
-//include( Pft_Config::getCfg('PATH_ROOT').'inc/view/footer.inc.php' );
+include( Pft_Config::getCfg('PATH_ROOT').'inc/view/footer.inc.php' );
 ?>
